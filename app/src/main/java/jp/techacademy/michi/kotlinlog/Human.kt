ed048db700aa9449e051349e89f7d94ede2e07ec
itoumichi <michi1008_ito@yahoo.co.jp>
2020-07-04 16:39:01 +0900

    AnimalクラスからHumanクラスへ

--- app/src/main/java/jp/techacademy/michi/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/michi/kotlinlog/Human.kt
@@ -3,8 +3,10 @@ package jp.techacademy.michi.kotlinlog
 import android.util.Log
 
 open class Human: Animal, Thinkable {
+    var hobby: String
 
-    constructor(name: String, age: Int, hobby: String) : super(name, age, hobby) {
+    constructor(name: String, age: Int, hobby: String) : super(name, age) {
+        this.hobby = hobby
 
     }
     override fun say(){
